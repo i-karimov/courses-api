2a3b84476a4857f09d2fe1a3f07d1d66f0388bd4
Ilmir Karimov <code.for.func@gmail.com>
2024-11-12 16:29:00 +0400

    doc(author): prepare todo reminders

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,7 +1,9 @@
 class Author < ApplicationRecord
-  before_destroy ->(_author) { raise "Move the logic of orphan courses` author reassignment logic to layer above" }
+  before_destroy ->(_author) { raise "Implement the logic of orphan courses` author reassignment on layer above" }
+
+  has_many :courses
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true # todo: validate format
 end
