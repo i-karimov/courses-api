2cf47e7fdb7861ddaa94961645e25af61ce1e714
Ilmir Karimov <code.for.func@gmail.com>
2024-11-12 16:31:11 +0400

    feat(authors): add api controller

---
+++ app/controllers/api/v1/authors_controller.rb
@@ -0,0 +1,41 @@
+class Api::V1::AuthorsController < ApplicationController
+  def index
+    @authors = Author.all
+    render json: @authors
+  end
+
+  def show
+    @author = Author.find(params[:id])
+    render json: @author
+  end
+
+  def create
+    @author = Author.new(author_params)
+    if @author.save
+      render json: @author, status: :created
+    else
+      render json: @author.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @author = Author.find(params[:id])
+    if @author.update_attributes(author_params)
+      render json: @author
+    else
+      render json: @author.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @author = Author.find(params[:id])
+    @author.destroy
+    head :no_content
+  end
+
+  private
+
+  def author_params
+    params.require(:author).permit(:first_name, :last_name, :email)
+  end
+end
