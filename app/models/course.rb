6f6a62a49570609673356cdeb223a3256eee6128
Ilmir Karimov <code.for.func@gmail.com>
2024-11-18 15:34:53 +0400

    fixup(model): author

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,7 @@
 class Course < ApplicationRecord
   belongs_to :author
 
-  has_many :course_skills
+  has_many :course_skills, dependent: :destroy
   has_many :skills, through: :course_skills
 
   validates :title, presence: true
