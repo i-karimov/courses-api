faffe4a1201a56a1407369ef98b677d2a6ff15d8
Ilmir Karimov <code.for.func@gmail.com>
2024-11-22 01:17:38 +0400

    chore(sidekiq): install

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
-  mount Rswag::Ui::Engine => '/api-docs'
-  mount Rswag::Api::Engine => '/api-docs'
+  mount Rswag::Ui::Engine => "/api-docs"
+  mount Rswag::Api::Engine => "/api-docs"
+  
   namespace :api do
     namespace :v1 do
-      resources :authors
+      resources :authors do
+        resources :courses, only: [ :create ]
+      end
+
+      resources :courses do
+        resources :skills, only: [ :create ]
+      end
+
+      resources :skills, only: [ :index, 
     end
   end
 end
