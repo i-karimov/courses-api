fe5f1249da85c2b62e3f6b9656f9419562caa091
Ilmir Karimov <code.for.func@gmail.com>
2024-11-22 20:19:06 +0400

    feat(author): add email validation

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,9 +1,7 @@
 class Author < ApplicationRecord
-  # before_destroy ->(_author) { raise "Implement the logic of orphan courses` author reassignment on layer above" }
-
   has_many :courses
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true, uniqueness: true # todo: validate format
+  validates :email, presence: true, uniqueness: true, 'valid_email2/email': true
 end
